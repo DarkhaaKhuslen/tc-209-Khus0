9a6adf6b4cd7b902f08c93f96df134429806ff5b
DarkhaaKhuslen <167779315+DarkhaaKhuslen@users.noreply.github.com>
2024-11-14 14:28:30 +0800

    1  1.5 deh

---
+++ app.js
@@ -0,0 +1,15 @@
+var a=3, b=3, c=3;
+console.log("a"+a+" b"+b+" c"+c);
+console.log("p="+"a+"+"b+"+"c");
+console.log("p="+"3+"+"3+"+"3="+(3*3));
+var too=55, too1=55;
+console.log("55+55="+(55+55));
+var a0=5, a1=5, a2=5;
+console.log("a=1 "+ "a=2 "+"a=3");
+console.log("s=(a*a)*6");
+console.log("s="+(5*5)*6);
+var a=6, b=4;
+var s,p
+console.log("p=(6+4)*2="+(2*10));
+console.log("s=6*4="+ (6*4));
+
